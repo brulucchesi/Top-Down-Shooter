120ebcf9ca91fe7acabb0c0232d5d98e034c5faa
Bruna Lucchesi <brunalvillela@gmail.com>
2020-09-13 22:57:08 -0300

    Project organization + fix Enemy Shooter patrol

---
+++ Top Down Shooter/Assets/Scripts/Ships/Enemies/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Ship
+{
+    protected Player _player;
+
+    [SerializeField]
+    private int _pointToPlayer = 1;
+
+    [Header("Field Of View")]
+    [SerializeField]
+    protected float _maxDistance = 4f;
+
+    [SerializeField]
+    protected float _maxAngle = 100f;
+
+    protected float _currentAngle;
+
+    protected float _currentDistance;
+
+    private EnemySpawnManager _enemySpawnManager;
+
+
+    protected virtual void OnEnable()
+    {
+        EnableShipCollider(true);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        _player = _gameManager.Player;
+
+        _enemySpawnManager = EnemySpawnManager.Instance;
+    }
+
+    protected override void FixedUpdate()
+    {
+        FieldOfView();
+        base.FixedUpdate();
+    }
+
+    protected virtual void FieldOfView()
+    {
+        _currentAngle = Vector3.SignedAngle(ShipObject.transform.up, _player.transform.position - transform.position, Vector3.forward);
+
+        _currentDistance = Vector3.Distance(_player.transform.position, transform.position);
+    }
+
+    protected override void Death()
+    {
+        base.Death();
+        
+        if(_currentHealth <= 0)
+            _gameManager.AddPointsToPlayer(_pointToPlayer);
+
+        ResetShip();
+    }
+
+    public override void ResetShip()
+    {
+        base.ResetShip();
+
+        gameObject.SetActive(false);
+        _enemySpawnManager.RemoveEnemy(gameObject);
+    }
+
+}
---
+++ Top Down Shooter/Assets/Scripts/Ships/Enemies/EnemyChaser.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChaser : Enemy
+{
+    private bool _canChase = false;
+
+    protected override void FieldOfView()
+    {
+        base.FieldOfView();
+
+        if (_currentDistance < _maxDistance && _currentAngle < _maxAngle && _currentAngle > -_maxAngle)
+        {
+            _canChase = true;
+        }
+        else
+        {
+            _canChase = false;
+        }
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+
+        if (!_canChase)
+        {
+            _rigidbody.velocity = Vector3.zero;
+            return;
+        }
+           
+        Vector3 direction = _player.transform.position - transform.position;
+
+        ShipObject.transform.up = Vector3.Lerp(ShipObject.transform.up, direction, Time.fixedDeltaTime * _rotateSpeed);
+
+        _rigidbody.velocity = ShipObject.transform.up * _moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _canChase = false;
+
+            GameObject o = collision.gameObject;
+            Ship ship = o.transform.GetComponent<Ship>();
+
+            if (ship != null)
+            {
+                ship.TakeDamage(DamageOther);
+            }
+
+            EnableShipCollider(false);
+            _animator.SetTrigger("Explode");
+        }
+    }
+}
